dae114332acb5a7dc01b60f74fc20c362a494901
U-fips\samba <johannes.unger@vstforx.de>
2019-10-21 23:54:40 +0200

    start player current file no sheet fallback impl.

--- out/commands/Play.js
+++ out/commands/Play.js
@@ -13,22 +13,43 @@ const ACommand_1 = require("./ACommand");
 const vscode = require("vscode");
 const path_1 = require("path");
 const Player_1 = require("../com/Player");
+const path = require("path");
+let lastSheetFile = null;
 class Play extends ACommand_1.ACommand {
+    startPlayer(sheetPath) {
+        let filename = path_1.basename(sheetPath);
+        let player = Player_1.getPlayer();
+        player.play(sheetPath)
+            .then(() => { })
+            .catch((ex) => {
+            vscode.window.showErrorMessage(`Werckmeister: ${ex}`);
+        });
+        vscode.commands.executeCommand('extension.werckmeister.sheetview');
+    }
+    isSheetFile(strPath) {
+        if (path.extname(strPath) === '.sheet') {
+            return true;
+        }
+        return false;
+    }
     execute() {
         return __awaiter(this, void 0, void 0, function* () {
             let editor = vscode.window.activeTextEditor;
             if (!editor) {
+                if (lastSheetFile !== null) {
+                    this.startPlayer(lastSheetFile);
+                }
                 return;
             }
             let sheetPath = editor.document.fileName;
-            let filename = path_1.basename(sheetPath);
-            let player = Player_1.getPlayer();
-            player.play(sheetPath)
-                .then(() => { })
-                .catch((ex) => {
-                vscode.window.showErrorMessage(`Werckmeister: ${ex}`);
-            });
-            vscode.commands.executeCommand('extension.werckmeister.sheetview');
+            if (!this.isSheetFile(sheetPath)) {
+                if (lastSheetFile !== null) {
+                    this.startPlayer(lastSheetFile);
+                }
+                return;
+            }
+            this.startPlayer(sheetPath);
+            lastSheetFile = sheetPath;
         });
     }
 }
--- src/commands/Play.ts
+++ src/commands/Play.ts
@@ -2,15 +2,13 @@ import { ACommand } from "./ACommand";
 import * as vscode from 'vscode';
 import { basename } from 'path';
 import { getPlayer, Player } from '../com/Player';
+import * as path from 'path';
+
+let lastSheetFile:string|null = null;
 
 export class Play extends ACommand {
 
-    async execute(): Promise<void> {
-        let editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
-        let sheetPath = editor.document.fileName;
+    startPlayer(sheetPath:string) {
         let filename = basename(sheetPath);
         let player:Player = getPlayer();
         player.play(sheetPath) 
@@ -19,5 +17,31 @@ export class Play extends ACommand {
             vscode.window.showErrorMessage(`Werckmeister: ${ex}`);
         });
         vscode.commands.executeCommand('extension.werckmeister.sheetview');
+    }
+
+    isSheetFile(strPath:string): boolean {
+        if (path.extname(strPath) === '.sheet') {
+            return true;
+        }
+        return false;
+    }
+
+    async execute(): Promise<void> {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            if (lastSheetFile !== null) {
+                this.startPlayer(lastSheetFile)
+            }
+            return;
+        }
+        let sheetPath = editor.document.fileName;
+        if (!this.isSheetFile(sheetPath)) {
+            if (lastSheetFile !== null) {
+                this.startPlayer(lastSheetFile)
+            }
+            return; 
+        }
+        this.startPlayer(sheetPath);
+        lastSheetFile = sheetPath;
     }
 }
