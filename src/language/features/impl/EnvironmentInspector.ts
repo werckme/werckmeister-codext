f176bd187068e4542ee614b4ab3c95145f97cfc8
Johannes Unger <unger@digitalklang.de>
2022-05-05 07:04:05 +0200

    fixing device list in windows

--- src/language/features/impl/EnvironmentInspector.ts
+++ src/language/features/impl/EnvironmentInspector.ts
@@ -1,12 +1,13 @@
 import { IEnvironmentInspector, MidiDeviceInfo } from "@werckmeister/language-features/IEnvironmentInspector";
 import { getPlayer } from "../../../com/Player";
+import { EOL } from 'os';
 
 export class EnvironmentInspector implements IEnvironmentInspector {
     public async getMidiOutputDevices(): Promise<MidiDeviceInfo[]> {
         try {
             const player = getPlayer();
             const devicesStr = await player.listDevices();
-            const devices = devicesStr.split('\n');
+            const devices = devicesStr.split(EOL);
             return devices
                 .filter(line => !!line && line.trim().length > 0)
                 .map(line => (line.match(/^(\d+):\s*(.*)$/) || []))
