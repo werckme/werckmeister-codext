e191eeae3f6dc7e7d0371703a03ed712dbe6c803
samba <samba@manjaro>
2022-10-24 21:56:50 +0200

    removing uneccesary piano view

--- WebViewApp/main.js
+++ WebViewApp/main.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { SheetViewComponent } from './components/sheetview/sheetView.component';
-import { PianoViewComponent } from './components/pianoview/pianoView.component';
 import { TransportViewComponent } from './components/transportView/transportView.component';
 import { DebuggerComponent } from './components/debugger/debugger.component';
 
@@ -13,10 +12,6 @@ const DebuggerId = 'debugger-main-component';
 const sheetView = document.getElementById(SheetViewId);
 if (sheetView) {
     ReactDOM.render(<SheetViewComponent />, sheetView);
-}
-const pianoView = document.getElementById(PianoViewId);
-if (pianoView) {
-    ReactDOM.render(<PianoViewComponent />, pianoView);
 }
 const transportView = document.getElementById(TransportViewId);
 if (transportView) {
